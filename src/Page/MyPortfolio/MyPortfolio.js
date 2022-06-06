5c554151dfe9b56155153fe50a6a964fbbd6437f
my name <myemailid@domain.com>
2022-06-05 22:36:29 -0400

    git change directiry

--- src/Page/MyPortfolio/MyPortfolio.js
+++ src/Page/MyPortfolio/MyPortfolio.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 const MyPortfolio = () => {
     return (
-        <div className='text-center mt-24'>
+        <div className=' mt-24 px-16'>
             <h2 className='text-6xl '>This is My Bio Data</h2>
             <div class="card max-w-6xl bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">My name  is Fayej Ahamed</h2>
-                    <p>Email: foyezfoyez017@gmail.com</p>
-                    <p>My educational background is: undergraduate drop out from science</p>
-                    <p>I am frontend Web developer. I am very highly Skilled in React Js, Also I can biuld  a dynamic Website. Also i am skilled in Single Page Application</p>
-                    <p>My project link:</p>
+                <div class="">
+                    <h2 class="card-title p-20">My name  is Fayej Ahamed</h2>
+                    <p className='mt-5 p-10'>Email: foyezfoyez017@gmail.com</p>
+                    <p className='mt-5 p-10'>My educational background is: undergraduate drop out from science</p>
+                    <p className='mt-5 p-10'>From excelling in various solutions-centric roles of increasing responsibilities, I have become adept at
+                        researching, diagnosing, troubleshooting, and identifying solutions to resolve system and network issues. My superior interpersonal and communication skills — in rapidly changing, ambiguous, and undefined environments — stand me out from the pack.</p>
+                    <p className='p-10'>I am frontend Web developer. I am very highly Skilled in React Js, Also I can biuld  a dynamic Website. Also i am skilled in Single Page Application</p>
+                    <p className='p-10'>I've had a passion for learning how things work and creating new ones for as long as I can remember. I have experience with Javascript, React, Node.js, Express, SQL  among others, and love to learn new tech. Currently,</p>
+                    <p className='p-10'>My project link:</p>
                     <a className='text-blue-700' href="https://ahamed-law-office.web.app/home">1:Law Office</a>
                     <a className='text-blue-700' href="https://stalwart-eclair-575422.netlify.app/">2:New dell release</a>
                     <a className='text-blue-700' href="https://electronics-warehouse-ef7bc.web.app/">3:Warehouse</a>
-                    
+
                 </div>
             </div>
         </div>
